4bbccead05d757b5d59422fb7e639a3ab24e17db
Hung Jin Lin <gae.m.project@gmail.com>
2017-04-02 18:21:59 +0800

    update required packages in setup

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     name='ilms-nthu',
     packages=['ilms'],
     install_requires=[
-        'requests'
+        'requests',
+        'beautifulsoup4',
+        'lxml'
     ],
-    version='0.0.1',
+    version='0.0.3',
     description='iLms-NTHU API. An iLMS client for stduents, assistants and developers.',
     author='leVirve',
     author_email='gae.m.project@gmail.com',
